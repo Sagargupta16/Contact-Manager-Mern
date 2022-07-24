431dd44c9333497e97e3bb6282b7bf4c780da3a2
Sagar Gupta <sg85207@gmail.com>
2022-07-24 10:24:27 +0530

    9th Commit

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const express = require('express');
 const connectDB = require('./config/db');
-var cors = require('cors');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 const contacts = require('./routes/api/contacts');
+
 
 const app = express();
 const path = require('path');
 
+dotenv.config();
+
 // Connect Database
 connectDB();
 
-app.use(cors());
+app.use(cors({origin: true,Credentials: true}));
 app.use(express.json());
 app.use('/api/contacts', contacts);
 
 __dirname = path.resolve();
 
 
-if (process.env.NODE_ENV === 'production') {
- app.use(express.static(path.join(__dirname,'client/build')));
- app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+ app.use(express.static(path.join(__dirname,"/client/build")));
+ app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"));
  });
-}
-else{
+}else{
     app.get('/', (req, res) => {
-        res.send('Server is running');
+        res.send('API is running');
     })
 }
 const port = process.env.PORT || 3006;
