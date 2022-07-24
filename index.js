ae774a283c79f26a7242053c7d0d74d63edbf984
Sagar Gupta <sg85207@gmail.com>
2022-07-24 08:34:34 +0530

    9th Commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/api/contacts', contacts);
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
  app.use(express.static('client/build'));
  app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname + '/client/build/index.html'));
+ res.sendFile(path.join(__dirname + '/client/build'));
  });
 }
 
