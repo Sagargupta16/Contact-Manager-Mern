8466d079587d73cb9529e0cb51332ae113a75bd0
Sagar Gupta <sg85207@gmail.com>
2022-07-24 08:32:12 +0530

    9th Commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 connectDB();
 
 app.use(cors({origin: true,Credentials: true}));
-
+app.use('/api/contacts', contacts);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
  app.use(express.static('client/build'));
@@ -19,11 +19,6 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
  res.sendFile(path.join(__dirname + '/client/build/index.html'));
  });
 }
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-app.use('/api/contacts', contacts);
-
 
 const port = process.env.PORT || 3006;
 
