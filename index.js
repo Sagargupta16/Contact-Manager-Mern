a45dc756562dd46323da9f4bc0af607560c727d9
Sagar Gupta <sg85207@gmail.com>
2022-07-24 09:10:16 +0530

    9th Commit

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
  app.use(express.static(path.join(__dirname,'client/build')));
-
  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
  });
