7c9aa4ef282b116146608bd789105b1bc348c057
Sagar Gupta <sg85207@gmail.com>
2022-07-24 08:23:22 +0530

    8th Commit

--- index.js
+++ index.js
@@ -17,13 +17,15 @@ app.get('/', (req, res) => res.send('Hello world!'));
 
 app.use('/api/contacts', contacts);
 
-const port = process.env.PORT || 3006;
 
-if (port === 'production' || port === 'staging') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
  app.use(express.static('client/build'));
  app.get('*', (req, res) => {
  res.sendFile(path.join(__dirname + '/client/build/index.html'));
  });
 }
+
+const port = process.env.PORT || 3006;
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
