e1aff6f46bcc0de6c510fd407bcdc63e47858b0a
Sagar Gupta <sg85207@gmail.com>
2022-07-24 08:09:34 +0530

    6th Commit

--- index.js
+++ index.js
@@ -12,17 +12,18 @@ connectDB();
 
 app.use(cors({origin: true,Credentials: true}));
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
- app.use(express.static('client/build'));
- app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname + '/client/build/index.html'));
- });
-}
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
 app.use('/api/contacts', contacts);
 
 const port = process.env.PORT || 3006;
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+ app.use(express.static('client/build'));
+ app.get('*', (req, res) => {
+ res.sendFile(path.join(__dirname + '/client/build/index.html'));
+ });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
