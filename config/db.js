2077c33af52baa7e54dadb6b980a56271d4fd13e
Sagar Gupta <sg85207@gmail.com>
2026-03-17 06:44:34 +0530

    feat: Add MongoDB connection setup and client package lock file.

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
+const db = process.env.MONGO_URI || config.get("mongoURI");
 
 const connectDB = async () => {
   try {
